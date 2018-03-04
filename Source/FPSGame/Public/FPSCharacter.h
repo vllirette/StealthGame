5195dabc2a59f7e072054e8253f19e6ccecdbe47
vincentLirette <vi.lirette@gmail.com>
2018-03-04 13:30:35 -0500

    Projectile and PlayerPitch replications

--- Source/FPSGame/Public/FPSCharacter.h
+++ Source/FPSGame/Public/FPSCharacter.h
@@ -60,6 +60,9 @@ protected:
 	/** Fires a projectile. */
 	void Fire();
 
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerFire();
+
 	/** Handles moving forward/backward */
 	void MoveForward(float Val);
 
@@ -74,6 +77,8 @@ public:
 
 	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return CameraComponent; }
+
+	virtual void Tick(float DeltaTime) override;
 
 };
 
